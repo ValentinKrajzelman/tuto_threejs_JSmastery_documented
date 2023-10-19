24ff1fe0c4674db9c111b195f3c34b72320e0a6f
ValentinKrajzelman <valentinkrajzelman@hotmail.com>
2023-10-19 20:54:43 -0300

    cosas

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+//esta es la pagina que se devuelve cuando se hace un catch de error en el programa
 export default function ErrorPage() {
   return <div>Something went wrong. Please refresh the page.</div>;
 }
